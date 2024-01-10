58cf3898b97d12664c96f113c5f0743505d1a31e
mina karam <minakaramkhalil.28@gmail.com>
2024-01-10 22:45:53 +0200

    owl

--- Home.js
+++ Home.js
@@ -2,14 +2,15 @@ $(".carousel1").owlCarousel({
     // loop: true,
     margin: 15,
     nav: true,
+    slideBy:4,
     dots: true,
     // autoplay: true,
     responsive: {
         0: {
-            items: 2,
+            items: 1,
         },
         610: {
-            items: 3,
+            items: 2,
         },
         1000: {
             items: 4,
