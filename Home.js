21a826f8a254604b244c6ab9309156221504fdf4
dalia.muhammed <dalia.muhammed1525@gmail.com>
2024-01-23 20:55:58 +0200

    adding join our staff table

--- Home.js
+++ Home.js
@@ -1,25 +1,25 @@
-var navbarToggler = document.getElementById("navbar-toggler-id");
-var navbarDropdown = document.getElementById("navbar-toggler-items");
+var navbarToggler = document.getElementById('navbar-toggler-id');
+var navbarDropdown = document.getElementById('navbar-toggler-items');
 // Toggle visibility when the toggler is clicked
-navbarToggler.addEventListener("click", (event) => {
+navbarToggler.addEventListener('click', event => {
   event.stopPropagation(); // Prevents the click event from reaching the document
   navbarDropdown.style.visibility =
-    navbarDropdown.style.visibility === "hidden" ? "visible" : "hidden";
+    navbarDropdown.style.visibility === 'hidden' ? 'visible' : 'hidden';
   navbarDropdown.style.opacity =
-    navbarDropdown.style.opacity === "0" ? "1" : "0";
+    navbarDropdown.style.opacity === '0' ? '1' : '0';
 });
 // Hide dropdown when clicking outside of it
-document.addEventListener("click", (event) => {
+document.addEventListener('click', event => {
   if (
-    navbarDropdown.style.visibility === "visible" &&
+    navbarDropdown.style.visibility === 'visible' &&
     !navbarDropdown.contains(event.target)
   ) {
-    navbarDropdown.style.visibility = "hidden";
-    navbarDropdown.style.opacity = "0";
+    navbarDropdown.style.visibility = 'hidden';
+    navbarDropdown.style.opacity = '0';
   }
 });
 // Stop propagation when clicking inside the dropdown
-navbarDropdown.addEventListener("click", (event) => {
+navbarDropdown.addEventListener('click', event => {
   event.stopPropagation(); // Prevents the click event from reaching the document
 });
 // ----------------------------------------------------------------------------------------------------------------
@@ -47,28 +47,28 @@ function initializeCarousel(carousel) {
     },
   });
 
-  let customDotsContainer = carousel.find(".owl-dots");
-  let customDots = customDotsContainer.find(".owl-dot");
+  let customDotsContainer = carousel.find('.owl-dots');
+  let customDots = customDotsContainer.find('.owl-dot');
   let maxVisibleDots = 4; // Set the maximum number of visible dots
 
   if (customDots.length > maxVisibleDots) {
     // Set the width for the extra dots to 10px
     for (let i = maxVisibleDots; i < customDots.length; i++) {
       // Get the span inside the dot and set its style
-      customDots.eq(i).find("span").css("width", "10px");
+      customDots.eq(i).find('span').css('width', '10px');
     }
   }
 }
 
-initializeCarousel($(".carousel1"));
-initializeCarousel($(".carousel2"));
+initializeCarousel($('.carousel1'));
+initializeCarousel($('.carousel2'));
 
 //------------------------------------ cities slider---------------------------------
 let currentIndex = 0;
 
 function showSlide(index) {
-  const slides = document.querySelector(".cities-wrapper");
-  const slideWidth = document.querySelector(".swiper-slide-cities").offsetWidth;
+  const slides = document.querySelector('.cities-wrapper');
+  const slideWidth = document.querySelector('.swiper-slide-cities').offsetWidth;
   const newPosition = -index * slideWidth;
   slides.style.transform = `translateX(${newPosition}px)`;
   currentIndex = index;
@@ -80,7 +80,7 @@ function showSlide(index) {
 function nextCitiesSlide() {
   currentIndex =
     (currentIndex + 1) %
-    document.querySelectorAll(".swiper-slide-cities").length;
+    document.querySelectorAll('.swiper-slide-cities').length;
   showSlide(currentIndex);
 }
 
@@ -88,36 +88,36 @@ function prevCitiesSlide() {
   currentIndex =
     (currentIndex -
       1 +
-      document.querySelectorAll(".swiper-slide-cities").length) %
-    document.querySelectorAll(".swiper-slide-cities").length;
+      document.querySelectorAll('.swiper-slide-cities').length) %
+    document.querySelectorAll('.swiper-slide-cities').length;
   showSlide(currentIndex);
 }
 
 function setSecondContainerWidth() {
-  const cityWidth = document.querySelector(".card-city-width");
+  const cityWidth = document.querySelector('.card-city-width');
   const cityCurrentWidth = cityWidth.offsetWidth;
 
-  const loadCourses = document.querySelector(".view-courses-width");
+  const loadCourses = document.querySelector('.view-courses-width');
   loadCourses.style.width = `${cityCurrentWidth}px`;
 }
 
 function updatePagination() {
-  const paginationContainer = document.querySelector(".pagination-container");
-  paginationContainer.innerHTML = ""; // Clear existing pagination
+  const paginationContainer = document.querySelector('.pagination-container');
+  paginationContainer.innerHTML = ''; // Clear existing pagination
 
-  const totalSlides = document.querySelectorAll(".swiper-slide-cities").length;
+  const totalSlides = document.querySelectorAll('.swiper-slide-cities').length;
 
   for (let i = 0; i < totalSlides; i++) {
-    const paginationItem = document.createElement("div");
-    paginationItem.classList.add("pagination-item");
+    const paginationItem = document.createElement('div');
+    paginationItem.classList.add('pagination-item');
 
     // Add a class to the active pagination item
     if (i === currentIndex) {
-      paginationItem.classList.add("active");
+      paginationItem.classList.add('active');
     }
 
     // Add a click event listener to each pagination tab
-    paginationItem.addEventListener("click", function () {
+    paginationItem.addEventListener('click', function () {
       showSlide(i);
     });
 
@@ -128,12 +128,12 @@ function updatePagination() {
 function updateSlideData() {
   // Update data based on currentIndex
   // Example: Fetch new data from an API and update the content
-  const cityNameElement = document.querySelector(".courses-city p");
-  const courseCountElement = document.querySelector(".courses-city-average");
+  const cityNameElement = document.querySelector('.courses-city p');
+  const courseCountElement = document.querySelector('.courses-city-average');
   // Update these elements based on your data
   const newData = {
-    cityName: "New City", // Example new city name
-    courseCount: "New Course Count", // Example new course count
+    cityName: 'New City', // Example new city name
+    courseCount: 'New Course Count', // Example new course count
   };
   cityNameElement.textContent = newData.cityName;
   courseCountElement.textContent = newData.courseCount;
@@ -144,24 +144,24 @@ setSecondContainerWidth();
 updatePagination();
 updateSlideData(); // Call the function to update data for the initial slide
 
-window.addEventListener("resize", setSecondContainerWidth);
+window.addEventListener('resize', setSecondContainerWidth);
 
 // category-courses-section----------------------------------------------------------------------------------
 
 // swiper-------------------------
 
-const mySwiper = new Swiper(".swiper-container1", {
+const mySwiper = new Swiper('.swiper-container1', {
   spaceBetween: 30,
   grid: {
     rows: 1,
   },
   pagination: {
-    el: ".swiper-pagination1",
+    el: '.swiper-pagination1',
     clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next", // CSS class or HTML element for next arrow
-    prevEl: ".swiper-button-prev", // CSS class or HTML element for prev arrow
+    nextEl: '.swiper-button-next', // CSS class or HTML element for next arrow
+    prevEl: '.swiper-button-prev', // CSS class or HTML element for prev arrow
   },
   breakpoints: {
     1300: {
@@ -191,26 +191,23 @@ const mySwiper = new Swiper(".swiper-container1", {
 
 // gallery Memory Section ----------------------------------------------------------------------
 function handleGalleryWidth() {
-  const container = document.getElementById("memorySectionContainer");
+  const container = document.getElementById('memorySectionContainer');
   const containerWidth = container.offsetHeight;
-  console.log(containerWidth);
-  const gallery = document.getElementById("AsideGallery");
+  const gallery = document.getElementById('AsideGallery');
 
   // Check if the screen width is larger than 1025px
   if (window.innerWidth > 1025) {
     gallery.style.height = `${containerWidth}px`;
-    const gal = gallery.style.height;
-    console.log(gal);
   }
 }
 
 handleGalleryWidth();
 
-window.addEventListener("resize", handleGalleryWidth);
+window.addEventListener('resize', handleGalleryWidth);
 
 // testimonial section ---------------------------------------------------------------------------------
 
-var swiper = new Swiper(".testimonialSwiper", {
+var swiper = new Swiper('.testimonialSwiper', {
   centeredSlides: true,
   initialSlide: 3,
   loop: true,
@@ -219,12 +216,12 @@ var swiper = new Swiper(".testimonialSwiper", {
     reverseDirection: true, // Set the delay to 10 seconds (in milliseconds)
   },
   pagination: {
-    el: ".swiper-pagination2",
+    el: '.swiper-pagination2',
     clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next2", // CSS class or HTML element for next arrow
-    prevEl: ".swiper-button-prev2", // CSS class or HTML element for prev arrow
+    nextEl: '.swiper-button-next2', // CSS class or HTML element for next arrow
+    prevEl: '.swiper-button-prev2', // CSS class or HTML element for prev arrow
   },
   breakpoints: {
     320: {
@@ -260,74 +257,74 @@ function updateImageShow() {
   // Check if the screen width is larger than 1024 pixels
   if (window.innerWidth > 1024) {
     var activeSlide = document.querySelector(
-      ".testimonial-section .swiper-slide.swiper-slide-active"
+      '.testimonial-section .swiper-slide.swiper-slide-active'
     );
     var allSlides = document.querySelectorAll(
-      ".testimonial-section .swiper-slide"
+      '.testimonial-section .swiper-slide'
     );
     var allSlidersTestimonialInfo = document.querySelectorAll(
-      ".testimonials-person-info"
+      '.testimonials-person-info'
     );
     var positionParagraphs = document.querySelectorAll(
-      ".testimonial-position-paragraph"
+      '.testimonial-position-paragraph'
     );
-    var testimonialName = document.querySelectorAll(".testimonial-name");
+    var testimonialName = document.querySelectorAll('.testimonial-name');
     var testimonialInfo;
     var testimonialTxt;
 
     allSlides.forEach(function (slide) {
       if (slide === activeSlide) {
-        slide.classList.add("active-testimonial");
+        slide.classList.add('active-testimonial');
       } else {
-        slide.classList.remove("active-testimonial");
-        slide.classList.add("notActiveTestimonial");
+        slide.classList.remove('active-testimonial');
+        slide.classList.add('notActiveTestimonial');
       }
     });
 
     if (activeSlide) {
-      testimonialInfo = activeSlide.querySelector(".testimonials-person-info");
+      testimonialInfo = activeSlide.querySelector('.testimonials-person-info');
       testimonialTxt = activeSlide.querySelector(
-        ".tesimonials-title-container"
+        '.tesimonials-title-container'
       );
 
       if (testimonialInfo) {
-        testimonialInfo.style.display = "block";
+        testimonialInfo.style.display = 'block';
       }
     }
 
     allSlidersTestimonialInfo.forEach(function (info) {
       if (info !== testimonialInfo) {
-        info.style.display = "none";
+        info.style.display = 'none';
       }
       document
-        .querySelectorAll(".tesimonials-title-container")
-        .forEach((title) => {
+        .querySelectorAll('.tesimonials-title-container')
+        .forEach(title => {
           if (title !== testimonialTxt) {
-            title.style.flexDirection = "column";
-            title.style.background = "transparent";
+            title.style.flexDirection = 'column';
+            title.style.background = 'transparent';
           } else {
-            title.style.flexDirection = "row";
-            title.style.background = "var(--Nuturals-N-6)";
+            title.style.flexDirection = 'row';
+            title.style.background = 'var(--Nuturals-N-6)';
           }
         });
     });
 
     positionParagraphs.forEach(function (paragraph) {
       if (!activeSlide.contains(paragraph)) {
-        paragraph.style.display = "none";
+        paragraph.style.display = 'none';
       } else {
-        paragraph.style.display = "block";
+        paragraph.style.display = 'block';
       }
     });
 
     testimonialName.forEach(function (name) {
       if (!activeSlide.contains(name)) {
-        name.style.color = "var(--NuturalsN-5)";
-        name.style.marginTop = "0.5rem";
-        name.style.fontSize = "1.1rem";
-        name.style.textAlign = "center";
+        name.style.color = 'var(--NuturalsN-5)';
+        name.style.marginTop = '0.5rem';
+        name.style.fontSize = '1.1rem';
+        name.style.textAlign = 'center';
       } else {
-        name.style.color = "var(--Nuturals-N-1)";
+        name.style.color = 'var(--Nuturals-N-1)';
       }
     });
   }
@@ -337,23 +334,22 @@ function updateImageShow() {
 updateImageShow();
 
 // Handle window resize to trigger the function
-window.addEventListener("resize", updateImageShow);
+window.addEventListener('resize', updateImageShow);
 // staff slider ----------------------------------------------------------------------------------------------------
 
-
-var myStaffSwiper = new Swiper(".staff-swiper", {
+var myStaffSwiper = new Swiper('.staff-swiper', {
   spaceBetween: 0,
   slidesPerView: 3,
   grid: {
     rows: 1,
   },
   pagination: {
-    el: ".staff-swiper-pagination",
+    el: '.staff-swiper-pagination',
     clickable: true,
   },
   navigation: {
-    nextEl: ".staff-button-next", // CSS class or HTML element for next arrow
-    prevEl: ".staff-button-prev", // CSS class or HTML element for prev arrow
+    nextEl: '.staff-button-next', // CSS class or HTML element for next arrow
+    prevEl: '.staff-button-prev', // CSS class or HTML element for prev arrow
   },
   breakpoints: {
     1300: {
@@ -381,11 +377,10 @@ var myStaffSwiper = new Swiper(".staff-swiper", {
   },
 });
 
-
 // international section swiper ------------------------------------------------------------------------------------
 
-var swiper = new Swiper(".international-swiper", {
-  effect: "coverflow",
+var swiper = new Swiper('.international-swiper', {
+  effect: 'coverflow',
   spaceBetween: 10,
   grabCursor: true,
   centeredSlides: true,
@@ -396,7 +391,7 @@ var swiper = new Swiper(".international-swiper", {
     delay: 5000, // Set the delay to 10 seconds (in milliseconds)
   },
   pagination: {
-    el: ".international-swiper-pagination",
+    el: '.international-swiper-pagination',
     clickable: true,
   },
   coverflowEffect: {
@@ -420,4 +415,89 @@ var swiper = new Swiper(".international-swiper", {
       spaceBetween: 50,
     },
   },
+});
+// ----------------------------------------------- Eurodemy offices ---------------------------------------
+let map = document.querySelector('iframe');
+const officesContainer = document.querySelector('.offices-container');
+const offices = Array.from(document.getElementsByClassName('office-container'));
+const downArrow = document.querySelector('.arrow-active-tab-container');
+const mapContainer = document.querySelector('.office-location');
+const mapDirectContainer = document.querySelector('.selected-office-map ');
+function handleMapHeight() {
+  const mapContainerHeight = mapContainer.offsetHeight;
+  const officesContainerHeight = officesContainer.offsetHeight;
+  const mapDirectContainerHeight = mapDirectContainer.offsetHeight;
+  const mapHeight = map.offsetHeight;
+  if (window.innerWidth >= 1025) {
+    mapContainer.style.height = `${officesContainerHeight}px`;
+  } else if (window.innerWidth < 1025) {
+    mapDirectContainer.style.height = `${mapHeight}px`;
+  }
+}
+
+handleMapHeight();
+
+window.addEventListener('resize', handleMapHeight);
+
+let activeOffice = null;
+
+const handleOffice = e => {
+  let office = e.currentTarget;
+
+  offices.forEach(otherOffice => {
+    if (otherOffice != office) {
+      otherOffice.style.background = '';
+      otherOffice.style.color = '';
+      otherOffice.img = '';
+      const existingImage = otherOffice.querySelector('.office-arrow-img');
+      if (existingImage) {
+        otherOffice.removeChild(existingImage);
+      }
+      downArrow.style.opacity = '0';
+    }
+  });
+  office.style.background = '#262121';
+  office.style.color = '#FCFCFC';
+  office.childNodes[3].style.opacity = '1';
+  if (window.innerWidth > 1024) {
+    const img = document.createElement('img');
+    img.className = 'office-arrow-img';
+    img.src = './Assets/location-arrow.svg';
+    img.style.position = 'absolute';
+    img.style.width = '30px';
+    img.style.height = '34px';
+    office.style.position = 'relative';
+    img.style.right = '-2.5rem';
+    img.style.top = '50%';
+    img.style.transform = 'translateY(-50%)';
+    const existingImage = office.querySelector('.office-arrow-img');
+    if (existingImage) {
+      office.removeChild(existingImage);
+    }
+    office.appendChild(img);
+  } else if (window.innerWidth < 1024) {
+    downArrow.style.opacity = '0';
+    downArrow.style.opacity = '1';
+  }
+  handleLocation(office.childNodes[1].innerText.toLowerCase());
+  activeOffice = office;
+};
+const handleLocation = cityName => {
+  const obj = {
+    spain:
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27133626.124768965!2d-28.316144682626973!3d33.88423937455913!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc42e3783261bc8b%3A0xa6ec2c940768a3ec!2sSpain!5e0!3m2!1sen!2seg!4v1705867862474!5m2!1sen!2seg',
+    beirut:
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52992.285824322964!2d35.426341460242064!3d33.889193762453!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151f17215880a78f%3A0x729182bae99836b4!2sBeirut%2C%20Lebanon!5e0!3m2!1sen!2seg!4v1705881232181!5m2!1sen!2seg',
+  };
+
+  map.src = obj[cityName];
+};
+window.addEventListener('resize', () => {
+  if (activeOffice) {
+    handleOffice({ currentTarget: activeOffice });
+  }
+});
+handleOffice({ currentTarget: offices[0] });
+offices.forEach(office => {
+  office.addEventListener('click', handleOffice.bind(this));
 });
