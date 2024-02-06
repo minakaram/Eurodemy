21fccb4413408bcffd4a6df7e264d36b183ee837
mina karam <minakaramkhalil.28@gmail.com>
2024-02-06 04:30:32 +0200

    a

--- Career Details page/CareerDetails.js
+++ Career Details page/CareerDetails.js
@@ -1,3 +1,92 @@
+
+//   navbar dropdown ----------------------------------------------------------------------------------
+
+// laptop screen
+const categories_Nav_Button = document.getElementById("active-list-id");
+const categories_Dropdown = document.getElementById("categories-dropdown-id");
+const categoriesSvgPaths = categories_Nav_Button.querySelectorAll("path");
+
+categories_Nav_Button.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const computedStyles = window.getComputedStyle(categories_Dropdown);
+  const visibility = computedStyles.getPropertyValue("visibility");
+
+  if (visibility === "hidden") {
+    categories_Dropdown.style.visibility = "visible";
+    categories_Dropdown.style.opacity = "1";
+    categoriesSvgPaths.forEach((path) => {
+      path.style.fill = "#ad1519";
+    });
+  } else {
+    categories_Dropdown.style.visibility = "hidden";
+    categories_Dropdown.style.opacity = "0";
+    categoriesSvgPaths.forEach((path) => {
+      path.style.fill = "black";
+    });
+  }
+});
+
+document.addEventListener("click", (e) => {
+  const isClickInsideDropdown =
+    categories_Nav_Button.contains(e.target) ||
+    categories_Dropdown.contains(e.target);
+
+  if (!isClickInsideDropdown) {
+    categories_Dropdown.style.visibility = "hidden";
+    categories_Dropdown.style.opacity = "0";
+    categoriesSvgPaths.forEach((path) => {
+      path.style.fill = "black";
+    });
+  }
+});
+
+//   cities dropdown -----------------------------------------
+const cities_Nav_Button = document.getElementById("cities-dropdown-button");
+const cities_Dropdown = document.getElementById("cities-dropdown-id");
+const citiesSvgPath = cities_Nav_Button.querySelectorAll("path");
+
+cities_Nav_Button.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const computedStyles = window.getComputedStyle(cities_Dropdown);
+  const visibility = computedStyles.getPropertyValue("visibility");
+
+  if (visibility === "hidden") {
+    cities_Dropdown.style.visibility = "visible";
+    cities_Dropdown.style.opacity = "1";
+    citiesSvgPath.forEach((path) => {
+      path.style.fill = "#ad1519";
+    });
+  } else {
+    cities_Dropdown.style.visibility = "hidden";
+    cities_Dropdown.style.opacity = "0";
+    citiesSvgPath.forEach((path) => {
+      path.style.fill = "black";
+    });
+  }
+});
+
+document.addEventListener("click", (e) => {
+  const isClickInsideDropdown =
+    cities_Nav_Button.contains(e.target) || cities_Dropdown.contains(e.target);
+
+  if (!isClickInsideDropdown) {
+    cities_Dropdown.style.visibility = "hidden";
+    cities_Dropdown.style.opacity = "0";
+    citiesSvgPath.forEach((path) => {
+      path.style.fill = "black";
+    });
+  }
+});
+
+
+
+
+
+
+
+
 const fileCvInput = document.querySelector("#file-cv-input");
 const fileCvImage = document.querySelector("#file-cv-image");
 const fileCvName = document.querySelector("#file-cv-name");
--- Careers/Careers.js
+++ Careers/Careers.js
@@ -1,13 +1,94 @@
-var myStaffSwiper = new Swiper('.swiper-container1', {
-  // initialSlide: 2,
-  // loop: true,
+//   navbar dropdown ----------------------------------------------------------------------------------
+
+// laptop screen
+const categories_Nav_Button = document.getElementById("active-list-id");
+const categories_Dropdown = document.getElementById("categories-dropdown-id");
+const categoriesSvgPaths = categories_Nav_Button.querySelectorAll("path");
+
+categories_Nav_Button.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const computedStyles = window.getComputedStyle(categories_Dropdown);
+  const visibility = computedStyles.getPropertyValue("visibility");
+
+  if (visibility === "hidden") {
+    categories_Dropdown.style.visibility = "visible";
+    categories_Dropdown.style.opacity = "1";
+    categoriesSvgPaths.forEach((path) => {
+      path.style.fill = "#ad1519";
+    });
+  } else {
+    categories_Dropdown.style.visibility = "hidden";
+    categories_Dropdown.style.opacity = "0";
+    categoriesSvgPaths.forEach((path) => {
+      path.style.fill = "black";
+    });
+  }
+});
+
+document.addEventListener("click", (e) => {
+  const isClickInsideDropdown =
+    categories_Nav_Button.contains(e.target) ||
+    categories_Dropdown.contains(e.target);
+
+  if (!isClickInsideDropdown) {
+    categories_Dropdown.style.visibility = "hidden";
+    categories_Dropdown.style.opacity = "0";
+    categoriesSvgPaths.forEach((path) => {
+      path.style.fill = "black";
+    });
+  }
+});
+
+//   cities dropdown -----------------------------------------
+const cities_Nav_Button = document.getElementById("cities-dropdown-button");
+const cities_Dropdown = document.getElementById("cities-dropdown-id");
+const citiesSvgPath = cities_Nav_Button.querySelectorAll("path");
+
+cities_Nav_Button.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const computedStyles = window.getComputedStyle(cities_Dropdown);
+  const visibility = computedStyles.getPropertyValue("visibility");
+
+  if (visibility === "hidden") {
+    cities_Dropdown.style.visibility = "visible";
+    cities_Dropdown.style.opacity = "1";
+    citiesSvgPath.forEach((path) => {
+      path.style.fill = "#ad1519";
+    });
+  } else {
+    cities_Dropdown.style.visibility = "hidden";
+    cities_Dropdown.style.opacity = "0";
+    citiesSvgPath.forEach((path) => {
+      path.style.fill = "black";
+    });
+  }
+});
+
+document.addEventListener("click", (e) => {
+  const isClickInsideDropdown =
+    cities_Nav_Button.contains(e.target) || cities_Dropdown.contains(e.target);
+
+  if (!isClickInsideDropdown) {
+    cities_Dropdown.style.visibility = "hidden";
+    cities_Dropdown.style.opacity = "0";
+    citiesSvgPath.forEach((path) => {
+      path.style.fill = "black";
+    });
+  }
+});
+
+var myStaffSwiper = new Swiper(".swiper-container1", {
+  initialSlide: 2,
+  loop: false,
   pagination: {
-    el: '.experience-swiper-pagination',
+    el: ".experience-swiper-pagination",
     clickable: true,
   },
   navigation: {
-    nextEl: '.experience-button-next', // CSS class or HTML element for next arrow
-    prevEl: '.experience-button-prev', // CSS class or HTML element for prev arrow
+    nextEl: ".experience-button-next", // CSS class or HTML element for next arrow
+    prevEl: ".experience-button-prev", // CSS class or HTML element for prev arrow
   },
   breakpoints: {
     1025: {
@@ -35,72 +116,66 @@ var myStaffSwiper = new Swiper('.swiper-container1', {
   on: {
     init: function () {
       const paginationContainer = document.querySelector(
-        '.experience-swiper-pagination'
+        ".experience-swiper-pagination"
       );
-      console.log(paginationContainer);
       const maxVisibleDots = 4; // Set the maximum number of visible dots
 
       const paginationDots = Array.from(paginationContainer.children);
 
-      // Hide dots more than 4 and replace them with an image
-      paginationDots.forEach((dot, index) => {
-        if (index >= maxVisibleDots) {
-          dot.style.display = 'none'; // Hide the dot
-          // Replace hidden dots with an image
-        }
-      });
-      const svgDots = document.createElement('div');
-      svgDots.style.display = 'flex';
-      svgDots.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="38" height="8" viewBox="0 0 38 8" fill="none">
-      <rect width="8" height="8" rx="1" fill="#F2EDED"/>
-      <rect x="10" width="8" height="8" rx="1" fill="#F2EDED"/>
-      <rect x="20" width="8" height="8" rx="1" fill="#F2EDED"/>
-      <rect x="30" width="8" height="8" rx="1" fill="#F2EDED"/>
-    </svg>`;
-      paginationContainer.appendChild(svgDots);
+      if (paginationDots.length > maxVisibleDots) {
+        // Hide dots more than 4 and replace them with an image
+        paginationDots.forEach((dot, index) => {
+          if (index >= maxVisibleDots) {
+            dot.style.display = "none"; // Hide the dot
+          }
+        });
+
+        const svgDots = document.createElement("div");
+        svgDots.style.display = "flex";
+        svgDots.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="38" height="8" viewBox="0 0 38 8" fill="none">
+          <rect width="8" height="8" rx="1" fill="#F2EDED"/>
+          <rect x="10" width="8" height="8" rx="1" fill="#F2EDED"/>
+          <rect x="20" width="8" height="8" rx="1" fill="#F2EDED"/>
+          <rect x="30" width="8" height="8" rx="1" fill="#F2EDED"/>
+        </svg>`;
+        paginationContainer.appendChild(svgDots);
+      }
     },
   },
 });
-// window.addEventListener('resize', () => {
-//   const swiperNextBtn = document.querySelector('.experience-button-next');
-//   const swiperPrevBtn = document.querySelector('.experience-button-prev');
-//   console.log(swiperPrevBtn);
-//   if (window.offsetWidth < 769) {
-//     swiperNextBtn.style.display = 'none';
-//   }
-// });
-// ------------------------------------------- form -------------------------------------------
-const fileCoverInput = document.querySelector('#file-cv-careers');
-const fileCoverImage = document.querySelector('#file-cover-image');
-const fileCoverName = document.querySelector('#cv-careers');
-const fileCvInput = document.querySelector('#cover-input-careers');
-const fileCvImage = document.querySelector('#file-cv-image');
-const fileCvName = document.querySelector('#cover-letter-input-careers');
 
-fileCoverImage.addEventListener('click', function () {
+// ------------------------------------------- form -------------------------------------------
+const fileCoverInput = document.querySelector("#file-cv-careers");
+const fileCoverImage = document.querySelector("#file-cover-image");
+const fileCoverName = document.querySelector("#cv-careers");
+const fileCvInput = document.querySelector("#cover-input-careers");
+const fileCvImage = document.querySelector("#file-cv-image");
+const fileCvName = document.querySelector("#cover-letter-input-careers");
+
+fileCoverImage.addEventListener("click", function () {
   fileCoverInput.click();
 });
-fileCvImage.addEventListener('click', function () {
+fileCvImage.addEventListener("click", function () {
   fileCvInput.click();
 });
-fileCoverInput.addEventListener('change', function () {
+fileCoverInput.addEventListener("change", function () {
   const file = fileCoverInput.files[0];
   if (file) {
     fileCoverName.value = file.name;
   } else {
-    fileCoverName.value = '';
+    fileCoverName.value = "";
   }
 });
-fileCvInput.addEventListener('change', function () {
+fileCvInput.addEventListener("change", function () {
   const file = fileCvInput.files[0];
   if (file) {
     fileCvName.value = file.name;
   } else {
-    fileCvName.value = '';
+    fileCvName.value = "";
   }
 });
-const clearCv = document.getElementById('clearCv');
+const clearCv = document.getElementById("clearCv");
 
-clearCv.addEventListener('click', () => {
-  fileCoverName.value = '';
+clearCv.addEventListener("click", () => {
+  fileCoverName.value = "";
 });
